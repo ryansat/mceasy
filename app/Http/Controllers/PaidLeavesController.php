35df63fd6e7951d20093336ade51efce0dabd848
Fajar Satria <fajarsat@hotmail.com>
2022-01-30 13:15:01 +0700

    Update PaidLeavesController.php
    update status pengecekan maksimal cuti

--- app/Http/Controllers/PaidLeavesController.php
+++ app/Http/Controllers/PaidLeavesController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Carbon\Carbon;
 use DB;
 use App\PaidLeaves;
 use Illuminate\Http\Request;
@@ -20,16 +20,22 @@ class PaidLeavesController extends Controller
 
     public function store(Request $request)
     {
-        $mostleave = DB::table('paid_leaves')
-        ->selectRaw('SUM(paid_leaves.lamacuti) as jumlahcuti')
-        ->groupBy('idpegawai')
-        ->havingRaw('idpegawai = ?', [$request->input('idpegawai')])
-        ->first();
+        $tahuncuti = $request->input('tglcuti');
+        $timestamp = strtotime($tahuncuti);
+        $date = date("Y", $timestamp);     
 
-        $jumlahleave = get_object_vars($mostleave);
+        $startDate = "{$date}-01-01";
+        $endDate = "{$date}-12-31";
+
+        $mostleave = DB::select(DB::raw("SELECT SUM(paid_leaves.lamacuti) as jumlahcuti FROM paid_leaves
+        where tglcuti between '{$startDate}' and '{$endDate}'
+        GROUP BY paid_leaves.idpegawai"));
+
+        $jumlahleave = array_column($mostleave, 'jumlahcuti');
+        $totalleave = intval($jumlahleave)+intval($request->input('lamacuti'));
 
         //check jika jumlah cuti yang diajukan melebihi yang tersisa
-        if ($jumlahleave['jumlahcuti']+$request->input('lamacuti') < 12){
+        if ($totalleave <= 12){
             $paidleaves = PaidLeaves::create([
                 'idpegawai'     => $request->input('idpegawai'),
                 'tglcuti'   => $request->input('tglcuti'),
